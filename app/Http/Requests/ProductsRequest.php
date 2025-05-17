ccda23e79ce3e190c026e33ac5f1aafd83aae746
Hanan Essam <11313356+hananessam@users.noreply.github.com>
2025-05-17 05:07:05 +0300

    limit get products validation

--- app/Http/Requests/ProductsRequest.php
+++ app/Http/Requests/ProductsRequest.php
@@ -15,9 +15,12 @@ class ProductsRequest extends FormRequest
     {
         return [
             'filters' => 'array',
-            'filters.*' => 'string',
+            'filters.name' => 'string|max:255',
+            'filters.price_min' => 'numeric|min:0',
+            'filters.price_max' => 'numeric|gte:filters.price_min',
             'sorts' => 'array',
-            'sorts.*' => 'string',
+            'sorts.price' => 'string|in:asc,desc',
+            'sorts.created_at' => 'string|in:asc,desc',
             'per_page' => 'integer|min:1|max:100',
             'page' => 'integer|min:1',
         ];
