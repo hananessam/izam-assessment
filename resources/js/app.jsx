7c9fc653b3787e8d9f29b9f72b49c1d1a37ca8f8
Hanan Essam <11313356+hananessam@users.noreply.github.com>
2025-05-18 21:10:51 +0300

    use light theme

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,12 +2,19 @@ import './bootstrap';
 
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import Home from './components/Home';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+    }
+});
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <BrowserRouter>
