6771c51cca1e880cd07cf0b283065e8cb52c312e
Hanan Essam <11313356+hananessam@users.noreply.github.com>
2025-05-18 21:08:13 +0300

    signin service

---
+++ resources/js/services/SignIn.jsx
@@ -0,0 +1,17 @@
+const SignIn = async (email, password) => {
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+
+    return response.json();
+};
+
+export default SignIn;
