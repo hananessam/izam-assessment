72732c06b7c0304f05fb072259e235d3bf88b335
Hanan Essam <11313356+hananessam@users.noreply.github.com>
2025-05-18 21:14:22 +0300

    handle protected routes functionality

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import Home from './components/Home';
+import Login from './components/Login';
+import Products from './components/Products';
+import AuthProvider from './hooks/AuthProvider';
+import PrivateRoute from './router/route';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -17,9 +21,18 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+        <CssBaseline /> 
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/products" element={<Products />} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    </ThemeProvider>
 );
--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -1,3 +1,23 @@
+import Button from '@mui/material/Button';
+import { useAuth } from '../hooks/AuthProvider';
+import { Navigate } from 'react-router-dom';
+
 export default function Home() {
-    return <h1 class="dark:text-white">Hello world</h1>;
+    const auth = useAuth();
+    const user = auth.user;
+
+    if (user) {
+        return <Navigate to="/products" />;
+    }
+
+    return <>
+        <div className="flex items-center justify-center">
+            <div className='me-4'>
+                <Button variant="outlined" href='/login'>Login</Button>
+            </div>
+            <div>
+                <Button variant="contained">Register</Button>
+            </div>
+        </div>
+    </>;
 }
