36e9981cc87d22ecd042c08beb90468600316cc2
Hanan Essam <11313356+hananessam@users.noreply.github.com>
2025-05-19 05:49:29 +0300

    protect products endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'products'], function () {
-    Route::get('/', \App\Http\Controllers\ProductController::class . '@index')->name('products.index');
-});
-
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/login', LoginController::class . '@login')->name('auth.login');
     Route::post('/logout', LoginController::class . '@logout')->name('auth.logout');
@@ -16,5 +12,9 @@ Route::group(['prefix' => 'auth'], function () {
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::group(['prefix' => 'auth'], function () {
         Route::get('/refresh-token', \App\Http\Controllers\Auth\AuthController::class . '@refreshToken')->name('auth.refresh-token');
+    });
+
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('/', \App\Http\Controllers\ProductController::class . '@index')->name('products.index');
     });
 });
